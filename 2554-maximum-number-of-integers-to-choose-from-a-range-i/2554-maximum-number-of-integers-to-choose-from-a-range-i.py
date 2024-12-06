10073188087a586a526cb9da742dffb4c2061bbf
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2024-12-06 11:15:18 -0800

    Time: 35 ms (92.98%), Space: 19.2 MB (7.35%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.py
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -1,33 +1,24 @@
 class Solution:
     def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
-        # Create a set for faster lookup of banned numbers
+        # create a banned_set
+        # Loop through the range(1, n+1) and skip the banned nums
+        # Check if the current sum is not bigger than maxSum.
+
         banned_set = set(banned)
-        
         current_sum = 0
         count = 0
-        
-        # Iterate through the range [1, n]
-        for num in range(1, n + 1):
-            if num not in banned_set and current_sum + num <= maxSum:
-                current_sum += num
+
+        for num in range(1, n+1):
+            if num in banned_set:
+                continue
+            if current_sum + num <= maxSum:
+                current_sum = current_sum + num
                 count += 1
+            elif current_sum == maxSum:
+                return count
+            else:
+                break
         
         return count
         
-
-        # #sliding Window
-        # left, right = 0, 1
-
-        # available = [i for i in range(1, n+1)]
-        # current_Max = 0
-        # current_Sum = 0
-
-        # while left < right:
-        #     window = available[left:right+1]
-        #     for num in window:
-        #         if num in banned:
-        #             window.remove(num)
-        #     if sum(window) < maxSum:
-        #         right += 1
-
         
