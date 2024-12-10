fd72f8078a7ee2e4f3237b9d5f93cf0e40ac9a92
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2024-12-09 20:50:33 -0800

    Time: 3 ms (40.32%), Space: 17.4 MB (8.53%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -6,33 +6,29 @@ class Solution:
         paths = []
         i = 0
 
-        # Create paths list, splitting the input path into meaningful parts
+        #create paths list, get rid of "/"s
         while i < len(path):
             if path[i] == "/":
                 i += 1
             else:
                 cur = ""
                 while i < len(path) and path[i] != "/":
-                    cur += path[i]
+                    cur = cur + path[i]
                     i += 1
                 paths.append(cur)
+    
 
-        # Stack to process the simplified path
         stack = []
-
-        # Process each part of the path
         for command in paths:
             if command == ".":
-                # Current directory, do nothing
                 continue
             elif command == "..":
-                # Move up one directory (pop from stack if not empty)
-                if stack:
+                if stack: # there is more commands
                     stack.pop()
             else:
-                # Valid directory name, add to stack
                 stack.append(command)
 
-        # Construct the simplified path
+        
+         # Construct the simplified path
         res = "/" + "/".join(stack)
         return res
