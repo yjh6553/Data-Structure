f58ee659085f9a70248278f87c050949301403ec
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2024-12-10 19:48:22 -0800

    Time: 12 ms (36.06%), Space: 18.1 MB (58.69%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.py
+++ 2924-find-champion-ii/2924-find-champion-ii.py
@@ -2,35 +2,19 @@ class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
         # Use Topological Sort
         indegree = [0] * n
-        adj = [[] for i in range(n)]
+
         for start, end in edges:
-            adj[start].append(end)
             indegree[end] += 1
         
-        # indegree = [0, 0, 2, 1]
-        # adj  = [[2], [2, 3], [], []]
-        
-        q = deque()
-        for node in range(n):
-            if indegree[node] == 0:
-                q.append(node)
-        
-        #q = [0, 1]
-
         champion = -1
-        count_zero_indegree = 0
+        champion_count = 0
 
-        while q:
-            if len(q) > 1:
-                return -1
-                
-            curr = q.popleft()
-            champion = curr
-            count_zero_indegree += 1 # Pop from que where it contains zero indegree nodes
-
-            for neighbor in adj[curr]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    q.append(neighbor)
+        for team in range(n):
+            if indegree[team] == 0:
+                champion = team
+                champion_count += 1
         
-        return champion if count_zero_indegree == 1 else -1
+        if champion_count == 1:
+            return champion
+        
+        return -1
