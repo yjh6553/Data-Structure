e8c364bf789757550a2dfef8c590cff06746ce2b
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2024-12-18 10:09:01 -0800

    Time: 166 ms (29.86%), Space: 39.7 MB (16.65%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -5,17 +5,11 @@ class MedianFinder:
         self.min_heap = []
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.max_heap, -num)
-        
-        # Ensure max_heap's largest element is smaller than min_heap's smallest
-        if self.max_heap and self.min_heap and (-self.max_heap[0] > self.min_heap[0]):
-            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
-        
-        # Balance the heaps
-        if len(self.max_heap) > len(self.min_heap) + 1:
-            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
-        elif len(self.min_heap) > len(self.max_heap):
-            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+        heappush(self.max_heap, -num)
+        heappush(self.min_heap, -heappop(self.max_heap))
+        if len(self.min_heap) > len(self.max_heap):
+            heappush(self.max_heap, -heappop(self.min_heap))
+
     
     def findMedian(self) -> float:
         if len(self.max_heap) == len(self.min_heap):
