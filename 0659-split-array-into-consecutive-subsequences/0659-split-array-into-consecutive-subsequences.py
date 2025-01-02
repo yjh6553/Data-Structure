589f71ad408f2b45eeee939a427ebcc46acf34a3
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-01-02 11:07:50 -0800

    Time: 20 ms (87.3%), Space: 18.8 MB (8.55%) - LeetHub

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
@@ -1,24 +1,19 @@
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
-        # [last_num, len_seq]
-        heap = []
-        for num in nums:
-            # 1.
-            while heap and heap[0][0] + 1 < num:
-                _, len_seq = heappop(heap)
-                if len_seq < 3:
+        counter = collections.Counter(nums)
+        for i in sorted(counter.keys()):
+            while counter[i] > 0:
+                last = 0
+                j = i
+                k = 0
+                while counter[j] >= last:
+                    last = counter[j]
+                    counter[j] -= 1
+                    j += 1
+                    k += 1
+                if k < 3:
                     return False
-            # 2.
-            if not heap:
-                heap.append([num, 1])
-            # 3.
-            else:
-                if heap[0][0] == num:
-                    heappush(heap, [num, 1])
-                else:
-                    _, len_seq = heappop(heap)
-                    heappush(heap, [num, len_seq + 1])
-        return all(p[1] >= 3 for p in heap)
+        return True
             
             
 
