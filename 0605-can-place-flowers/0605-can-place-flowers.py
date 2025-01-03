001b4c24350b8d35667e85b5ccef587daa74bf60
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-01-02 19:06:22 -0800

    Time: 14 ms (17.81%), Space: 18.3 MB (9.37%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        for i in range(len(flowerbed)):
+            left = i == 0 or flowerbed[i - 1] == 0
+            right = i == len(flowerbed) - 1 or flowerbed[i + 1] == 0
+
+            if left and right and flowerbed[i] == 0:
+                flowerbed[i] = 1
+                n -= 1
+        
+        return True if n <= 0 else False
+
+
+        # need to check left side is 0 and right side is 0
+        # When we create a 
