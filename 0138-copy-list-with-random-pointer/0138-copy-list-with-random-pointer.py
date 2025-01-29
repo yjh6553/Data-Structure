d4be99eb4d73d0be8395dde4ec459f2432e1d190
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-01-29 11:14:10 -0800

    Time: 44 ms (33.41%), Space: 18.5 MB (60.78%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -33,40 +33,5 @@ class Solution:
         return node_map[head]
 
 
-        
-        # if not head:
-        #     return None  # Handle edge case when head is None
-        
-        # dummy = Node(0)
-        # temp = head
 
-        # node_list = []
-        # index = 0
-
-        # # Create a list of deep copied nodes with values only
-        # while temp is not None:  # Fixed loop condition
-        #     new_copy = Node(temp.val)
-        #     node_list.append((new_copy, index))
-        #     temp = temp.next
         
-        # if not node_list:  # Handle edge case when node_list is empty
-        #     return None
-        
-        # dummy.next = node_list[0][0]
-
-        # # Update next pointer
-        # for i in range(len(node_list) - 1):  
-        #     node_list[i][0].next = node_list[i+1][0]
-        
-        # # Update random pointer
-        # origin = head
-        # update = dummy.next
-        # while origin is not None:  # Fix incorrect loop condition
-        #     if origin.random is not None:
-        #         random_index = [i for i, (node, _) in enumerate(node_list) if node.val == origin.random.val]
-        #         if random_index:
-        #             update.random = node_list[random_index[0]][0]  # Fix assignment to copied node
-        #     origin = origin.next
-        #     update = update.next  # Move to the next copied node
-
-        # return dummy.next
