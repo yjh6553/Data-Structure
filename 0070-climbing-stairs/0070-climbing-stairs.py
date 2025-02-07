18fd62535fc536bababc85377ddde86294fe9436
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-02-07 11:35:58 -0800

    Time: 0 ms (100%), Space: 17.7 MB (43%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # def helper(steps_remaining: int) -> int:
+        #     if steps_remaining == 0:
+        #         return 1
+        #     if steps_remaining < 0:
+        #         return 0
+            
+        #     return helper(steps_remaining - 1) + helper(steps_remaining - 2)
+
+        # return helper(n)
+
         if n == 1:
             return 1
 
