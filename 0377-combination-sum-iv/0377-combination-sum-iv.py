5e730956036aade8cc9b684e9fc0ec87251187aa
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-02-10 11:43:34 -0800

    Time: 43 ms (56.5%), Space: 17.8 MB (60.9%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,5 +1,6 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
+        # Top down: Target-------------
         memo = {}
         def dfs(cur: int) -> int:
             if cur == target:
@@ -14,28 +15,23 @@ class Solution:
             for num in nums:
                 count += dfs(cur + num)  # Recursively explore with updated cur
             memo[cur] = count
+
             return count
 
         return dfs(0)
 
+        # Tabulation, Target 1, target 2
 
-        # memo = {}
+        dp = [0] * (target + 1) # Index: subproblem target, value: number of cases that sums up to index.
+        dp[0] = 1  # Base case: One way to make sum 0 (do nothing)
 
-        # def dfs(remaining: int) -> int:
-        #     if remaining == 0:
-        #         return 1
-        #     if remaining < 0:
-        #         return 0
+        for t in range(1, target + 1):  # Compute solutions for each sum up to target
+            for num in nums:
+                if t >= num:  # Ensure we don't go out of bounds
+                    dp[t] += dp[t - num]  # Add ways from smaller sums
 
-        #     if remaining in memo:
-        #         return memo[remaining]
-        #     count = 0
-        #     for num in nums:
-        #         count += dfs(remaining - num)
+        return dp[target]  # Final answer is stored in dp[target]
 
-        #     memo[remaining] = count
-        #     return count
 
-        # return dfs(target)
 
 
