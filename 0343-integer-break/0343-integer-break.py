08a9603c352a64696f980fd4a232e0d779d30260
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-02-11 11:28:30 -0800

    Time: 3 ms (50.55%), Space: 17.8 MB (53.38%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -2,20 +2,19 @@ class Solution:
     def integerBreak(self, n: int) -> int:
         memo = {}
 
-        def dp(num):
+        def helper(num):
             if num in memo:
                 return memo[num]
-            if num == 2:
-                return 1  # The only valid way to split 2 is 1+1
-            
-            ans = 0  # Start with 0 since we want the maximum product
-            for i in range(1, num):  # Split `num` into `i` and `num - i`
-                ans = max(ans, i * (num - i), i * dp(num - i))  # Maximize the product
-            
-            memo[num] = ans  # Store computed result
+            if num <= 3:
+                return num
+
+            ans = num
+            for i in range(2, num):
+                ans = max(ans, i * helper(num - i))
+                
+            memo[num] = ans
             return ans
 
         if n <= 3:
-            return n - 1  # Special case: integer break requires at least one split
-        
-        return dp(n)
+            return n - 1
+        return helper(n)
