846c1572fb2ffc8e1ff874af40c3b0371e3d34bd
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-02-11 21:39:30 -0800

    Time: 4413 ms (10.89%), Space: 24.9 MB (18.09%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,32 +1,3 @@
-# class Solution:
-#     def numSquares(self, n: int) -> int:
-#         # perfect squares: 1*1, 2*2, 3*3, 4*4, 5*5
-#         # find the largest perfect square that is less than n
-
-#         # Match with other perfect sqaures that is less than the largest perfect squares number that we find.
-
-#         #find the availabe numbers to find n
-#         count = 1
-#         availableNums = []
-#         while count**2 < n:
-#             availableNums.append(count)
-#             count += 1
-       
-#         #[1, 4, 9]
-#         #Find a combination
-#         def findCombinations(nums: list, target: int, current: int) -> int:
-#             if current == target:
-#                 return 0
-#             if current > target:
-#                 return float('inf')
-#             exclude = 1 + findCombinations(nums[1:], target, current + nums[1:][0]) #[4, 9]
-#             include = 1 + findCombinations(nums, target, current + nums[0]) 
-#             result = min(include, exclude)
-#             return result
-
-#         return findCombinations(availableNums, n, 0)
-
-
 class Solution:
     def numSquares(self, n: int) -> int:
         # Generate all perfect squares less than or equal to n
