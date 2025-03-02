00d8373cf4e5393450ea5d62959da7d51337a1aa
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-03-01 19:53:07 -0800

    Time: 179 ms (49.46%), Space: 31.3 MB (64.81%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -7,18 +7,14 @@ class Solution:
         dq = deque()  
 
         for i in range(len(nums)):
-            # Remove elements outside the window
             if dq and dq[0] < i - k + 1:
                 dq.popleft()
 
-            # Remove elements smaller than the current element
             while dq and nums[dq[-1]] < nums[i]:
                 dq.pop()
 
-            # Add the current element's index
             dq.append(i)
 
-            # Append the maximum to the result once the first window is formed
             if i >= k - 1:
                 result.append(nums[dq[0]])
 
