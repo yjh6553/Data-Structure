004bdfbcc4df86ef3b4a1725fc5a40c1699c4a87
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-03-10 19:10:39 -0700

    Time: 3878 ms (28.33%), Space: 18 MB (32.99%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.py
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -8,19 +8,26 @@ class Solution:
         
         sides = [0] * 4  # To store lengths of four sides
         
-        def backtrack(index):
+
+        # [3, 3, 2, 1, 1, 1, 1, 1, 1, 1]
+        # edge_length = 4
+        # [4 4 4 4]
+        # index = 4
+        # cur_lenght = 0
+        # 
+        def backtrack(index) -> bool:
             if index == len(matchsticks):
                 return all(side == edge_length for side in sides)
             
-            for i in range(4):
-                if sides[i] + matchsticks[index] <= edge_length:
-                    sides[i] += matchsticks[index]
-                    if backtrack(index + 1):
+            for i in range(4): # i = 1
+                if sides[i] + matchsticks[index] <= edge_length: # 4 + 1 <= 4
+                    sides[i] += matchsticks[index] 
+                    if backtrack(index + 1): #backtrack(10)
                         return True
-                    sides[i] -= matchsticks[index]  # Backtrack
+                    sides[i] -= matchsticks[index] #sides[0] -= math  
                 
-                if sides[i] == 0:
-                    break  # Optimization: Avoid trying the same empty side multiple times
+                # if sides[i] == 0:  #side[0] = 3
+                #     break  # Optimization: Avoid trying the same empty side multiple times
             
             return False
         
