7e61419dce6d558683f8fc3515d4908587ecca7a
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-04-07 23:42:23 -0700

    Time: 155 ms (24.73%), Space: 68.1 MB (25.86%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,34 +1,19 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        # n = len(questions)
-        # memo = {}
-
-        # def helper(index: int) -> None:
-        #     # Base Case
-        #     if index >= n:
-        #         return 0
-        #     if index in memo:
-        #         return memo[index]
-            
-        #     take = questions[index][0] + helper(index + 1 + questions[index][1])
-        #     skip = helper(index + 1)
-        #     memo[index] = max(take, skip)
-            
-        #     return memo[index]
-
-        # return helper(0)
-
-
         n = len(questions)
-        dp = [0] * (n + 1)
+        memo = {}
 
-        for i in range(n - 1, -1, -1):
-            points, brainpower = questions[i]
-            next_q = i + 1 + brainpower
+        def helper(index: int) -> None:
+            # Base Case
+            if index >= n:
+                return 0
+            if index in memo:
+                return memo[index]
+            
+            take = questions[index][0] + helper(index + 1 + questions[index][1])
+            skip = helper(index + 1)
+            memo[index] = max(take, skip)
+            
+            return memo[index]
 
-            take = points + (dp[next_q] if next_q < n else 0) 
-            skip = dp[i + 1]
-
-            dp[i] = max(take, skip)
-        
-        return dp[0]
+        return helper(0)
