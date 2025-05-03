e1f76b254b4ca9933409fb4b6a1ef3b5321d9b2e
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-05-03 11:55:57 -0700

    Time: 61 ms (37.76%), Space: 18.5 MB (33.33%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -1,40 +1,11 @@
 class Solution:
-    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        tops_counts = Counter(tops)
-        bottom_counts = Counter(bottoms)
-        n = len(tops)
-        candidates = []
-        res = float('inf')
-    
-        for num in range(1, 7):
-            top_value = 0
-            bottom_value = 0
-            if num in tops_counts:
-                top_value = tops_counts[num]
-            if num in bottom_counts:
-                bottom_value = bottom_counts[num]
-            
-            if top_value + bottom_value >= n:
-                candidates.append(num)
-        
-        if not candidates:
-            return -1
-        
-        for candidate in candidates:
-            cur_res = 0
-            moveToTop = True
-            if tops_counts[candidate] <= bottom_counts[candidate]:
-                moveToTop = False
-
-            for index in range(n):
-                if tops[index] != candidate and bottoms[index] != candidate:
-                    return -1
-                elif moveToTop and tops[index] != candidate and bottoms[index] == candidate:
-                    cur_res += 1
-                elif not moveToTop and tops[index] == candidate and bottoms[index] != candidate:
-                    cur_res += 1
-            
-            res = min(res, cur_res)
-        
-        return res
-
+    def minDominoRotations(self, A: List[int], B: List[int]) -> int:
+        if len(A) != len(B): return -1
+        same, countA, countB = Counter(), Counter(A), Counter(B)
+        for a, b in zip(A, B):
+            if a == b:
+                same[a] += 1
+        for i in range(1, 7):
+            if countA[i] + countB[i] - same[i] == len(A):
+                return min(countA[i], countB[i]) - same[i]        
+        return -1
