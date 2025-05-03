42448cb883d53874230184051e94606085393628
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-05-03 12:04:22 -0700

    Time: 55 ms (45.57%), Space: 18.6 MB (13.29%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -1,21 +1,39 @@
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        tops_counts = Counter(tops)
+        bottom_counts = Counter(bottoms)
+        n = len(tops)
+        candidates = []
         res = float('inf')
+    
+        for num in range(1, 7):
+            top_value = 0
+            bottom_value = 0
+            if num in tops_counts:
+                top_value = tops_counts[num]
+            if num in bottom_counts:
+                bottom_value = bottom_counts[num]
+            
+            if top_value + bottom_value >= n:
+                candidates.append(num)
         
-        for val in range(1, 7):
-            top_swaps = bottom_swaps = 0
-            valid = True
+        if not candidates:
+            return -1
+        
+        for candidate in candidates:
+            cur_res = 0
+            moveToTop = True
+            if tops_counts[candidate] <= bottom_counts[candidate]:
+                moveToTop = False
 
-            for t, b in zip(tops, bottoms):
-                if t != val and b != val:
-                    valid = False
-                    break
-                if t != val:
-                    top_swaps += 1
-                
-                if b != val:
-                    bottom_swaps += 1
-            if valid:
-                res = min(res, top_swaps, bottom_swaps)
+            for index in range(n):
+                if tops[index] != candidate and bottoms[index] != candidate:
+                    return -1
+                elif moveToTop and tops[index] != candidate and bottoms[index] == candidate:
+                    cur_res += 1
+                elif not moveToTop and tops[index] == candidate and bottoms[index] != candidate:
+                    cur_res += 1
+            
+            res = min(res, cur_res)
         
-        return -1 if res == float('inf') else res
+        return res
