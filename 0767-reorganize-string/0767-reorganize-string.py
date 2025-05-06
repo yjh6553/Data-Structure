5022b681d0f7da9b011e7fc0361e62fe75a7f128
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-05-06 11:38:56 -0700

    Time: 7 ms (6.59%), Space: 17.9 MB (47.07%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,39 +1,27 @@
-from collections import defaultdict
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        freqDic = {}
-        for char in s:
-            freqDic[char] = freqDic.get(char, 0) + 1
-        
-        #create max_heap
-        max_heap = [(-freq, char) for char, freq in freqDic.items()] # -freq because python does not have maxHeap
+        count = Counter(s)
+        max_heap = []
+        for char, freq in count.items():
+            max_heap.append((-freq, char))
         heapq.heapify(max_heap)
 
         res = ""
 
-        while len(max_heap) >=2:
-            freq1, char1 = heapq.heappop(max_heap)
-            freq2, char2 = heapq.heappop(max_heap)
-
-            res += char1
-            res += char2
-
-            if freq1 + 1 < 0:
-                heapq.heappush(max_heap, (freq1 + 1, char1))
-            if freq2 + 1 < 0:
-                heapq.heappush(max_heap, (freq2 + 1, char2))
-        
-        if max_heap:
+        while max_heap:
             freq, char = heapq.heappop(max_heap)
-            if -freq > 1:
+            if len(res) == 0 or res[-1] != char:
+                res += char
+                if freq + 1 != 0:
+                    heapq.heappush(max_heap, (freq + 1, char))
+            elif res[-1] == char and max_heap:
+                second = heapq.heappop(max_heap)
+                res += second[1] 
+                if second[0] + 1 != 0:
+                    heapq.heappush(max_heap, (second[0] + 1, second[1]))
+                heapq.heappush(max_heap, (freq, char))
+            else:
                 return ""
-            res += char
-
-        return res
-
-
-
-
+            print(res)
         
-
-                
+        return res
