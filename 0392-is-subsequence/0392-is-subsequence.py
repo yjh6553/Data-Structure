a3d45f651a4e45e55befd318e133ba97020f103a
Joshua Yoo <65704935+yjh6553@users.noreply.github.com>
2025-05-09 11:19:40 -0700

    Time: 0 ms (100%), Space: 17.8 MB (67.62%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,15 +1,13 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        def helper(s_index: int, t_index: int) -> bool:
-            # Base cases
-            if s_index == len(s):  # All characters in s matched
+        index = 0
+        if not s:
+            return True
+
+        for char in t:
+            if index < len(s) and char == s[index]:
+                index += 1
+            if index == len(s):
                 return True
-            if t_index == len(t):  # t is exhausted
-                return False
-            
-            if s[s_index] == t[t_index]:
-                return helper(s_index + 1, t_index + 1)  
-            else:
-                return helper(s_index, t_index + 1)  
-        
-        return helper(0, 0)
+
+        return index == len(s)
